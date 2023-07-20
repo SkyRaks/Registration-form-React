85c207036eeb3a3b5e1044e1601024849c17c246
SkyRaks <nano.volosin2016@gmail.com>
2023-07-20 18:35:57 -0400

    every time new table

---
+++ src/main/java/com/oliver/Registrationform/controller/UserController.java
@@ -0,0 +1,56 @@
+package com.oliver.Registrationform.controller;
+
+import com.oliver.Registrationform.model.User;
+import com.oliver.Registrationform.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@org.springframework.stereotype.Controller
+public class UserController {
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/register")
+    public String getRegister() {
+        return "register";
+    }
+
+    @GetMapping("/login")
+    public String getLogin() {
+        return "login";
+    }
+
+    @GetMapping("/success")
+    public String getSuccess() {
+        return "success";
+    }
+
+    @GetMapping("/fail")
+    public String getFail() {
+        return "fail";
+    }
+
+    @PostMapping("/register")
+    public String register(User user) {
+        int savedUser = userRepository.saveUser(user);
+        if (savedUser > 0) {
+            return "redirect:/success";
+        } else {
+            return "redirect:/fail";
+        }
+    }
+
+    @PostMapping("/login")
+    public String login(User user) {
+        int foundUser = userRepository.findUser(user);
+        if (foundUser > 0) {
+            return "redirect:/success";
+        } else {
+            return "redirect:/fail";
+        }
+    }
+}
