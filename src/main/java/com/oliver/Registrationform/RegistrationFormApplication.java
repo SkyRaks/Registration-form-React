0c2b2778ca43cc9d886a4d79748b337ac8ee9b6f
SkyRaks <nano.volosin2016@gmail.com>
2023-07-28 11:35:00 -0400

    final

--- src/main/java/com/oliver/Registrationform/RegistrationFormApplication.java
+++ src/main/java/com/oliver/Registrationform/RegistrationFormApplication.java
@@ -3,11 +3,17 @@ package com.oliver.Registrationform;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collections;
+
 @SpringBootApplication
 public class RegistrationFormApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RegistrationFormApplication.class, args);
+//		SpringApplication.run(RegistrationFormApplication.class, args);
+		int port = Integer.parseInt(System.getenv("PORT"));
+		SpringApplication app = new SpringApplication(RegistrationFormApplication.class);
+		app.setDefaultProperties(Collections.singletonMap("server.port", String.valueOf(port)));
+		app.run(args);
 	}
 
 }
